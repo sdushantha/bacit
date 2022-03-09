8e6610a474da7a3ccc4453c23ea9d170469fd4d2
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2022-03-09 09:49:41 +0100

    Wed Mar  9 09:48:45 AM CET 2022

--- is-110/mod-7/src/com/siddharth/Main.java
+++ is-110/mod-7/src/com/siddharth/Main.java
@@ -1,19 +1,24 @@
 package com.siddharth;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         String[] fruits = {"apple", "banana", "coconut", "dates", "eggplant", "fig", "grapes"};
 
         String stopAt = "eggplant";
 
         System.out.println("There are " + fruits.length + " fruits");
 
-        for (int i = 0; i < fruits.length; i++) {
+        for (int i = 0; i<fruits.length; i++) {
             if (fruits[i].equals(stopAt)) {
                 System.out.println("Sorry, we need to stop at " + stopAt);
                 break;
             }
             System.out.println("Adding " + fruits[i] + " to basket");
         }
+
+        int a = 6;
+        int b = 12;
+        String out = a==b ? "Yes":"No";
+        System.out.println("IS 6 equal to 12?" + out);
     }
 }
